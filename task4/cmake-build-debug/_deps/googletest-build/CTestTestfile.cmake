8a9c46fad6ca7791d3b785394739f62351a3639b
Supyness <ev.sklyarenko@gmail.com>
2022-04-21 23:10:24 +0300

    Done Task5

--- task4/cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evgen/TP_tasks_new/Supilcat1/task4/cmake-build-debug/_deps/googletest-src
-# Build directory: /home/evgen/TP_tasks_new/Supilcat1/task4/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- task4/cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evgen/TP_tasks_new/Supilcat1/task4/cmake-build-debug/_deps/googletest-src/googlemock
-# Build directory: /home/evgen/TP_tasks_new/Supilcat1/task4/cmake-build-debug/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
